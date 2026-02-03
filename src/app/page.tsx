9730ccd822d23d28964cb2e43ec8c058d0287200
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-02-03 17:41:25 +0300

    add: görseller eklenerek giriş sayfası tamamlandı

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 // import LoginPage from "./login/login-page";
 // import { useUser } from "@/context/UserContext";
 
+import { ressam, runner, walker } from "../utils/";
+
 const Home = () => {
   // const { user } = useUser();
   const router = useRouter();
@@ -17,7 +19,7 @@ const Home = () => {
 
   return (
     <div className="relative">
-      <nav className="fixed top-0 left-0 w-full text-black z-10 px-20 py-5 2xl:py-10">
+      <nav className="fixed top-0 left-0 w-full text-black z-10 md:px-20 sm:px-10 px-5 py-5 2xl:py-10">
         <div className="w-full flex items-center justify-between">
           <div className="flex items-center gap-1 2xl:text-4xl">
             <span>sahnesen</span>
@@ -35,25 +37,19 @@ const Home = () => {
           </div>
         </div>
       </nav>
-      <div className="min-h-screen w-full flex items-center overflow-hidden relative px-20 bg-[#F7F4EA]">
+      <div className="min-h-screen w-full flex items-center overflow-hidden relative md:px-20 sm:px-10 px-5 bg-[#F7F4EA]">
         <div className="flex-1 h-screen w-full flex flex-col justify-between">
-          <div className="w-full flex items-center 2xl:justify-start h-full">
+          <div className="w-full flex items-center 2xl:justify-start h-full z-100">
             <div className="flex flex-col gap-5">
-              <div>
-                <div className="text-7xl 2xl:text-[120px]">
-                  <h1 className="font-quicksand">
-                    <span className="bg-gradient-to-r from-[#1a1a1a] to-[#064232] text-transparent bg-clip-text">
-                      Sahnedeysen
-                    </span>
-                    ,{" "}
-                    <span className="bg-gradient-to-r from-[#1a1a1a] to-[#064232] text-transparent bg-clip-text">
-                      sahnesen
-                    </span>
-                    .
-                  </h1>
-                </div>
+              <div className="flex flex-col font-quicksand text-[clamp(3rem,5vw,6rem)] 2xl:text-7xl leading-[clamp(3.5rem,6vw,5rem)]">
+                <h1 className="bg-gradient-to-r from-[#1a1a1a] to-[#064232] text-transparent bg-clip-text">
+                  Sahnedeysen
+                </h1>
+                <h1 className="bg-gradient-to-r from-[#1a1a1a] to-[#064232] text-transparent bg-clip-text">
+                  sahnesen.
+                </h1>
               </div>
-              <div className="text-sm 2xl:text-3xl w-120 2xl:w-250">
+              <div className="text-sm 2xl:text-3xl sm:w-120 w-60 2xl:w-250">
                 <p className="text-gray-500 font-quicksand">
                   Çalışmalarını paylaş, hobilerini anlat, başkalarının
                   sahnelerine konuk ol. Çünkü burada herkesin bir sahnesi var
@@ -75,6 +71,34 @@ const Home = () => {
               Gizlilik
             </li>
           </ul>
+        </div>
+        <div className="flex-1 sm:block hidden z-20">
+          <div className="relative lg:right-0 md:right-10 sm:right-20 right-0">
+            <div className="absolute -top-30 2xl:-top-40 2xl:left-20 xl:left-30 lg:left-25 -translate-y-1/2 2xl:h-180 h-[clamp(300px,32vw,450px)] 2xl:w-full w-[clamp(600px,80vh,1000px)] rounded-xl overflow-hidden shadow-md shadow-black/70 rotate-15">
+              <div className="absolute inset-0 bg-black opacity-70"></div>
+              <Image
+                src={ressam}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute -top-5 2xl:-top-20 2xl:-left-40 xl:-left-15 lg:left-20 -translate-y-1/2 2xl:h-100 2xl:w-150 h-[clamp(300px,10vw,700px)] w-[clamp(400px,10vh,600px)] rounded-xl overflow-hidden shadow-md shadow-black/70 z-10 rotate-40 sm:block hidden">
+              <div className="absolute inset-0 bg-black/50 opacity-70"></div>
+              <Image
+                src={walker}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="absolute top-40 2xl:top-60 xl:left-30 lg:left-40 -translate-y-1/2 2xl:h-180 h-[clamp(350px,32vw,400px)] 2xl:w-full w-[clamp(600px,80vh,1000px)] rounded-xl overflow-hidden shadow-md shadow-black/70 -rotate-10 z-50">
+              <div className="absolute inset-0 bg-black/50 opacity-20"></div>
+              <Image
+                src={runner}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
         </div>
       </div>
       {/* {showLoginModal && (
---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+import ressam from "../../public/images/ressam.jpeg";
+import runner from "../../public/images/runner.jpeg";
+import walker from "../../public/images/walker.jpeg";
+
+export { ressam, runner, walker };
