9fc58e4e331a22cd3e0a8172810025c06184dbe4
kutukalan <118997325+umutkutukalan@users.noreply.github.com>
2026-02-03 17:03:19 +0300

    create nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main>
+        <p className="text-5xl">
+          Hadi Başlayalım | Sahnesen
+        </p>
+      </main>
+    </div>
+  );
+}
